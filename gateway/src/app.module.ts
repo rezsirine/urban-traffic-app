bd7687bfe532fb7a941ff189049fe14315e2d7f4
Sirine Rezgui <sirine.rezgui@ensi-uma.tn>
2026-05-19 18:35:58 +0100

    gateway done

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,35 +1,28 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApolloGateway, RemoteGraphQLDataSource } from '@apollo/gateway';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
       driver: ApolloGatewayDriver,
       gateway: {
-        buildService({ url }) {
-          return new RemoteGraphQLDataSource({
-            url,
-            willSendRequest({ request, context }) {
-              // Transmettre le token JWT aux sous-services
-              request.http.headers.set(
-                'authorization',
-                context.req?.headers?.authorization || ''
-              );
-            },
-          });
-        },
-        serviceList: [
-          { name: 'auth',          url: process.env.AUTH_SERVICE_URL },
-          { name: 'vehicles',      url: process.env.VEHICLE_SERVICE_URL },
-          { name: 'traffic',       url: process.env.TRAFFIC_SERVICE_URL },
-          { name: 'incidents',     url: process.env.INCIDENT_SERVICE_URL },
-          { name: 'notifications', url: process.env.NOTIF_SERVICE_URL },
-        ],
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'auth', url: process.env.AUTH_SERVICE_URL || 'http://localhost:3001/graphql' },
+            { name: 'vehicles', url: process.env.VEHICLE_SERVICE_URL || 'http://localhost:3002/graphql' },
+            { name: 'traffic', url: process.env.TRAFFIC_SERVICE_URL || 'http://localhost:3003/graphql' },
+            { name: 'incidents', url: process.env.INCIDENT_SERVICE_URL || 'http://localhost:3004/graphql' },
+            { name: 'notifications', url: process.env.NOTIF_SERVICE_URL || 'http://localhost:3005/graphql' },
+          ],
+        }),
       },
-      context: ({ req }) => ({ req }),
     }),
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
